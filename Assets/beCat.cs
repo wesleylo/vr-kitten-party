ec5e5f7834961d9582ee675f8e622f2703f12864
wesleylo <weslo404@gmail.com>
2017-03-27 19:50:21 -0400

    Clean up.

--- Assets/beCat.cs
+++ Assets/beCat.cs
@@ -6,7 +6,7 @@ public class beCat : StateMachineBehaviour {
     private int rndState;
     // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
     //override public void OnStateEnter(Animator anim, AnimatorStateInfo stateInfo, int layerIndex) {
-
+    //
     //}
 
     // OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
@@ -30,26 +30,19 @@ public class beCat : StateMachineBehaviour {
 
     // OnStateMachineEnter is called when entering a statemachine via its Entry Node
     override public void OnStateMachineEnter(Animator anim, int stateMachinePathHash) {
-        //if (anim.GetBool("beingCat") == false || anim.GetBool("isIdle") == true)
-        //{
-        //    OnStateMachineExit(anim, stateMachinePathHash);
-        //} else
-        //{
-            anim.SetBool("isRunning", false);
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isIdle", false);
-            anim.SetBool("beingCat", true);
-            rndState = Random.Range(0, 5);
-            if (rndState == 4) rndState = 0; // More likely to go to idle
-            anim.SetInteger("beingCatState", rndState);
-            Debug.Log("StateMachine Enter = " + anim.GetInteger("beingCatState"));
-        //}
-
+        anim.SetBool("isRunning", false);
+        anim.SetBool("isWalking", false);
+        anim.SetBool("isIdle", false);
+        anim.SetBool("beingCat", true);
+        rndState = Random.Range(0, 5);
+        if (rndState == 4) rndState = 0; // More likely to go to idle
+        anim.SetInteger("beingCatState", rndState);
+        Debug.Log("StateMachine Enter = " + anim.GetInteger("beingCatState"));
     }
 
 	// OnStateMachineExit is called when exiting a statemachine via its Exit Node
 	override public void OnStateMachineExit(Animator anim, int stateMachinePathHash) {
-        anim.SetInteger("beingCatState", -1); // means exited
+        anim.SetInteger("beingCatState", -1); // Means exited
         anim.SetBool("isIdle", true);
         Debug.Log("StateMachine Exit");
     }
--- Assets/chase.cs
+++ Assets/chase.cs
@@ -21,12 +21,12 @@ public class chase : MonoBehaviour {
 
         rndInterest = Random.Range(0, 10);
         // Determine interestLvl: 1(0-5), 2(6-8), or 3(9)
-        
         if (rndInterest < 6) { // Cat will be interested and run
             interestLvl = 0;
             anim.SetBool("isIdle", true);
             anim.SetBool("beingCat", false);
             goToDist = Random.Range(0.5f, 1.5f);
+            Debug.Log("Is interested");
         } else if (rndInterest < 9)
         { // Cat will be interested and walk
             interestLvl = 1;
@@ -40,6 +40,7 @@ public class chase : MonoBehaviour {
             anim.SetBool("beingCat", true);
             Debug.Log("is uninterested");
         }
+
         Debug.Log("interestLvl = " + interestLvl);
         Debug.Log("gotoDist = " + goToDist);
 
@@ -49,13 +50,13 @@ public class chase : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        
+        // Toggle interest
         if ((Time.fixedTime % 9) == 0) // Every 11 sec (to go through < 1 idle)
         {
             if (anim.GetBool("isRunning") == false && anim.GetBool("isWalking") == false && anim.GetBool("isIdle") == true && anim.GetInteger("beingCatState") == -1)
             {
                 rndInterest = Random.Range(0, 11);
-                rndInterest = 0;
+                rndInterest = 1;
                 Debug.Log("rndInterest = " + rndInterest);
                 if (rndInterest == 0 || rndInterest == 1)
                 {
@@ -95,18 +96,19 @@ public class chase : MonoBehaviour {
             anim.SetBool("isIdle", false);
             anim.SetBool("beingCat", true);
         }
-        // randomly toggle beingCat here
+        // \Toggle interest
+
+
         if (anim.GetBool("beingCat") == true) {
             // Should only go into if not moving (ie. idle)
             if(anim.GetInteger("beingCatState") == -1)
             {
                 anim.SetBool("isIdle", true); // Getting reset after state machine exit for some reason
-            }
-            
+            }  
         }
         else {
             // Vector3.Distance(a,b) is the same as (a-b).magnitude
-            if (Vector3.Distance(player.position, this.transform.position) < 3) { // NPC in player influence circle
+            if (Vector3.Distance(player.position, this.transform.position) < 3) { // NPC is in player influence circle
                 Vector3 direction = player.position - this.transform.position; // Line between cat and player
                 direction.y = 0;
 
